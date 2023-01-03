720a3e8849276b72d47c34cd5ce9124544f01dc0
Ameya Panditrao <60094625+AmeyaP29@users.noreply.github.com>
2023-01-03 11:26:33 +0530

    feat and style: enabled translation, added style
    Added styling to the page, and linked the I/O to an external API

--- app.js
+++ app.js
@@ -1,7 +1,49 @@
-console.log("External JS linking");
+// console.log("External JS linking");
 
-alert("Alert demo");
+// alert("Alert demo");
 
-let userName = prompt("What's the username?");
+// let userName = prompt("What's the username?");
 
-alert(`Welcome to the website ${userName}`);
+// alert(`Welcome to the website ${userName}`);
+
+const buttonTranslate = document.querySelector("#btn-translate");
+
+const serverURL = "https://api.funtranslations.com/translate/minion.json";
+
+let textInput = document.querySelector("#input-text");
+
+function createTranslationURL(input) {
+
+return (serverURL + "?text=" + input);
+
+}
+
+let outputDiv = document.querySelector("#output-box");
+
+function clickHandler() {
+
+    let inputText = textInput.value;
+
+    fetch(createTranslationURL(inputText))
+    .then( response => response.json() )
+    .then( json => {
+
+    let translatedText = json.contents.translated;
+    outputDiv.innerText = translatedText;
+
+    })
+    .catch(errorHandler)
+
+}
+
+function errorHandler(error) {
+
+    console.log(error);
+    alert("The API that translates the text on this website is rate limited to 5 requests/hour. You have sent too many. Please wait an hour before trying again");
+
+
+}
+
+
+
+buttonTranslate.addEventListener("click", clickHandler);
